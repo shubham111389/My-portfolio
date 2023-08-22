68322e4e390369964d4bf74b7d02e224ef323233
shubham annpurne <shubhamannpurne1029@gmail.com>
2023-08-22 18:59:10 +0530

    change the  projects components

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -20,32 +20,33 @@ const Projects = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
         <ProjectsCard
           title="MY-YOUTUBE CLONE 👨‍💻"
-          des="📺 Welcome to my React-powered YouTube clone! Discover content with ease through advanced search 🔍, engage in live streaming discussions with real-time comments 💬, and immerse yourself in a visually stunning UI 🌈. Join me in revolutionizing online entertainment! 🚀🎉🎥 "
+          des="📺 Welcome to my React-powered YouTube clone! Discover content with ease through advanced search , engage in live streaming discussions with real-time comments , and immerse yourself in a visually stunning UI . Join me in revolutionizing online entertainment! 🚀🎉🎥 "
           src={projectOne}
         />
-        <ProjectsCard
-          title="Food_haven-Web_app"
-          des=" 📺 Welcome to our React-powered FOOD_Haven🍔 Web-app Discover your favourite restaurnat with ease through advanced search 🔍,  Add Favourate dishes to your cart 🛒 and login 🔑 hassle-free.,Enjoy a yummy design 🍽️ while you explore a world of flavors! 🚀👨‍🍳🎉"
-          src={projectTwo}
-        />
+        
         <ProjectsCard
           title="LiveCric🏏- extension"
-          des=" 🏏 Introducing the LiveCric Chrome Extension powered by React! Stay updated with live match scores in a flash ⚡. Never miss a moment of the game as you easily track the scores right from your browser. Stay in the loop with the excitement of cricket matches! 🚀🏆📊"
+          des=" 🏏 Introducing the LiveCric Chrome Extension powered by React! Stay updated with live match scores in a flash . Never miss a moment of the game as you easily track the scores right from your browser. Stay in the loop with the excitement of cricket matches! 🚀🏆📊"
           src={projectThree}
         />
         <ProjectsCard
+          title="Food_haven-Web_app"
+          des=" 📺 Welcome to our React-powered FOOD_Haven Web-app Discover your favourite restaurnat with ease through advanced search ,  Add Favourate dishes to your cart  and login  hassle-free.,Enjoy a yummy design  while you explore a world of flavors! 🚀👨‍🍳🎉"
+          src={projectTwo}
+        />
+        <ProjectsCard
           title="MEMSET - Web Game"
-          des=" Memset is a fun and engaging game that challenges your memory and cognitive abilities. In the game, you'll be presented with a grid of cards, each with a hidden picture or symbol on it. Your goal is to uncover pairs of matching cards as quickly as possible, using your memory and concentration skills to remember where each card is located. 🃏🔍💪"
+          des=" 💻Memset is a fun and engaging game that challenges your memory and cognitive abilities. In the game, you'll be presented with a grid of cards, each with a hidden picture or symbol on it. Your goal is to uncover pairs of matching cards as quickly as possible, using your memory and concentration skills to remember where each card is located. 🃏🔍"
           src={projectThree}
         />
         <ProjectsCard
           title="Basic bank-system 💻" 
-          des=" 💰 Welcome to the React-powered Basic Banking System 💻! Seamlessly manage your money with this Firebase-integrated app. Send and track transactions with ease, while enjoying a sleek and user-friendly UI. Experience modern banking made simple! 🚀💸📈"
+          des=" 💰 Welcome to the React-powered Basic Banking System ! Seamlessly manage your money with this Firebase-integrated app. Send and track transactions with ease, while enjoying a sleek and user-friendly UI. Experience modern banking made simple! 🚀💸📈"
           src={projectOne}
         />
         <ProjectsCard
           title="Portfolio 🚀"
-          des="🎨 Explore my Portfolio Showcase 📁! Crafted with care using React and personalized design, it's a hub of my creative journey. Discover my projects, skills, and experiences with an elegant and intuitive UI. Join me in sharing my passion and achievements! 🚀🌟👩‍💻"
+          des="🎨 Explore my Portfolio Showcase ! Crafted with care using React and personalized design, it's a hub of my creative journey. Discover my projects, skills, and experiences with an elegant and intuitive UI. Join me in sharing my passion and achievements! 🚀🌟👩‍💻"
           src={projectTwo}
         />
       </div>
